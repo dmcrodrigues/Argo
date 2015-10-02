4a82e4577c3568953d837b621883c252d6bb4713
Tony DiPasquale <tony@thoughtbot.com>
2015-10-02 13:35:59 -0700

    Re-organize operators into separate files
    This moves the monadic operators into their own files and renames the
    files with the Argo operators to (ReturnType)Pull.swift.

---
+++ Argo/Operators/Functor.swift
@@ -0,0 +1,14 @@
+/**
+  map a function over a `Decoded` value
+
+  - If the value is is a failing case (`.TypeMismatch`, `.MissingKey`), the function will not be evaluated and this will return the value
+  - If the value is `.Success`, the function will be applied to the unwrapped value
+
+  - parameter f: A transformation function from type `T` to type `U`
+  - parameter x: A value of type `Decoded<T>`
+
+  - returns: A value of type `Decoded<U>`
+*/
+public func <^> <T, U>(f: T -> U, x: Decoded<T>) -> Decoded<U> {
+  return x.map(f)
+}
